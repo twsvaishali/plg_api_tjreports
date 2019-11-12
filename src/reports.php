ef66aa65e8c2f414aabeec6c9d58a1b277b23e54
vaishali k <vaishali_k@techjoomla.com>
2019-11-12 17:21:56 +0530

    commit

--- src/reports.php
+++ src/reports.php
@@ -4,7 +4,7 @@
  * @subpackage   com_api
  *
  * @author       Techjoomla <extensions@techjoomla.com>
- * @copyright    Copyright (C) 2009 - 2018 Techjoomla. All rights reserved.
+ * @copyright    Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.
  * @license      GNU General Public License version 2 or later; see LICENSE.txt
  */
 
