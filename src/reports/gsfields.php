0470242a0716398cedd5697fd978b587794a323c
vaishali k <vaishali_k@techjoomla.com>
2020-01-03 12:21:11 +0530

    Issue #155552 task:Create a resource in tjreports API plugin to get the fields compatible with google DS for a report

---
+++ src/reports/gsfields.php
@@ -0,0 +1,69 @@
+﻿ <?php
+/**
+ * @package      Tjreports
+ * @subpackage   com_api
+ *
+ * @author       Techjoomla <extensions@techjoomla.com>
+ * @copyright    Copyright (C) 2009 - 2018 Techjoomla. All rights reserved.
+ * @license      GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+/**
+ * Tjreports API Gs fields class
+ * This is used to get the fields for the Google studio connector
+ *
+ * @since  1.0.0
+ */
+class ReportsApiResourceGsfields extends ApiResource
+{
+	/**
+	 * Function get filters data
+	 *
+	 * @return boolean
+	 */
+	public function get()
+	{
+		$app        = JFactory::getApplication();
+		$jinput     = $app->input;
+		$formData   = $jinput->post;
+		$reportName = $app->input->getString('id');
+
+		if (empty($reportName))
+		{
+			$reportName = $formData->getString('report');
+		}
+
+		if (!isset($reportName))
+		{
+			ApiError::raiseError(400, JText::_('PLG_API_REPORTS_REPORT_NAME_MISSSING'), 'APIValidationException');
+		}
+
+		// Create object of tjreports plugin class
+		JLoader::import('plugins.tjreports.' . $reportName . "." . $reportName, JPATH_SITE);
+		$className = 'TjreportsModel' . ucfirst($reportName);
+
+		if (!class_exists($className))
+		{
+			ApiError::raiseError(400, JText::_('PLG_API_REPORTS_REPORT_NAME_INVALID'), 'APIValidationException');
+		}
+
+		$reportPlugin = new $className;
+
+		// If plugin does not have getGoogleDsFields throw error
+		if (!method_exists($reportPlugin, 'getGoogleDsFields'))
+		{
+			ApiError::raiseError(400, JText::_('PLG_API_REPORTS_REPORT_NO_GOOGLESTUDIO_SUPPORT'), 'APIValidationException');
+		}
+
+		// Load language files
+		$lang = JFactory::getLanguage();
+		$lang->load('com_tjreports', JPATH_ADMINISTRATOR, 'en-GB', true);
+		$lang->load('plg_tjreports_' . $reportName, JPATH_SITE . "/plugins/tjreports/" . $reportName, 'en-GB', true);
+
+		$fields = $reportPlugin->getGoogleDsFields();
+		$this->plugin->setResponse($fields);
+	}
+}
